41764342e5b16aeadfcaf8762baedec787e941cf
Davidov249 <davidov249@gmail.com>
2017-10-04 16:13:24 -0600

    message

--- LaboratorioEncripcion/RSA.cs
+++ LaboratorioEncripcion/RSA.cs
@@ -8,5 +8,35 @@ namespace LaboratorioEncripcion
 {
     class RSA
     {
+        public int P { get; private set; }
+        public int Q { get; private set; }
+        public int N { get; private set; }
+        public int Fi { get; private set; }
+        public int e { get; private set; }
+        public int d { get; private set; }
+
+        public RSA(int p, int q)
+        {
+            P = p;
+            Q = q;
+        }
+
+        public void Obtenern()
+        {
+            N = P * Q;
+        }
+
+        public void Obtenerfi()
+        {
+            Fi = (P - 1) * (Q - 1);
+        }
+
+        public void Obtenere(int numero)
+        {
+            e = numero;
+        }
+
+
+
     }
 }
