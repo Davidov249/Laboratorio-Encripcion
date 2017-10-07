014f94440c24ca0cd01d93671942fdd077473203
Davidov249 <davidov249@gmail.com>
2017-10-06 22:19:41 -0600

    Ya verifica si son primos

--- LaboratorioEncripcion/RSA.cs
+++ LaboratorioEncripcion/RSA.cs
@@ -21,8 +21,36 @@ namespace LaboratorioEncripcion
             Q = q;
             Obtenern();
             Obtenerfi();
-            Obtenere(p);
+            Obtenere(3);
             Obtenerd();
+        }
+
+        public bool VerificarPrimos(int p, int q)
+        {
+            int var = 0;
+            int var2 = 0;
+            for (int i = 1; i < p + 1; i++)
+            {
+                if (p % i == 0)
+                {
+                    var++;
+                }
+            }
+            for (int i = 1; i < q + 1; i++)
+            {
+                if (p % i == 0)
+                {
+                    var2++;
+                }
+            }
+            if (var == 2 && var2 == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public void Obtenern()
