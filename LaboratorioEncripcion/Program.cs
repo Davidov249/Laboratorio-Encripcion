169c5877029af7fcf34fb3748e5b1c0aa7547fde
Davidov249 <davidov249@gmail.com>
2017-10-10 09:10:05 -0600

    message

--- LaboratorioEncripcion/Program.cs
+++ LaboratorioEncripcion/Program.cs
@@ -12,7 +12,8 @@ namespace LaboratorioEncripcion
         {
             char[] caracteres = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
             BloqueAES bloque = new BloqueAES(caracteres);
-            RSA Privada = new RSA(17, 23);
+            RSA Privada = new RSA(1117, 1123);
+            string llave = Privada.llavePrivada();
             Console.ReadKey();
         }
     }
